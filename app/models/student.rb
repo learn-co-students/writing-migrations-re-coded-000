d43beeda453632efcdbdb35dc7ae6ecd66891c20
ALI ALRAWI <ali.developer90@gmail.com>
2016-11-14 08:55:26 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
   def self.create_table
 
-ActiveRecord::Base.connection.execute("create table if not exists students (id INTEGER PRIMARY KEY,name TEXT)")
+   ActiveRecord::Base.connection.execute("create table if not exists students (id INTEGER PRIMARY KEY,name TEXT)")
 
   end
 end
